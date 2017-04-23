ae76f830a12173f4da0d66306784a17614becc90
Renato Maia <maia@tecgraf.puc-rio.br>
2017-04-23 18:36:42 -0300

    Implementação do requisito opicional de ignorar itens vazios.

--- angular/assignment1/js/app.js
+++ angular/assignment1/js/app.js
@@ -1,17 +1,25 @@
+"use strict";
+
 (function () {
 	function LunchCheckController($scope) {
 		$scope.input = "";
 		$scope.checkInput = function () {
-			if ($scope.input === "")
+			if ($scope.input === "") {
 				$scope.output = "Please enter data first";
-			else if ($scope.input.split(',', 4).length > 3)
-				$scope.output = "Too much!";
-			else
-				$scope.output = "Enjoy!";
+			} else {
+				var count = 0;
+				var items = $scope.input.split(",");
+				for (var i=0; i<items.length; ++i) {
+					if (items[i].search(/\S/) !== -1) {
+						++count;
+					}
+				}
+				$scope.output = count>3 ? "Too much!" : "Enjoy!";
+			}
 		};
 	}
 	LunchCheckController.$inject = ["$scope"];
-	angular.module('LunchCheck', []).controller("LunchCheckController",
+	angular.module("LunchCheck", []).controller("LunchCheckController",
 	                                            LunchCheckController);
 })()
 
